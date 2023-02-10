3280f1f688048b87f24924837bff3877e675bf3a
Fernando A <fernando.augustogomesp@gmail.com>
2023-02-10 16:12:18 -0300

    feat: aws lambda function code

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-require('dotenv').config()
-const express = require('express')
-const routes = require('./src/main.js')
+const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3')
+const hubspot = require('./src/hubspot.js')
+const csv = require('./src/spreadsheet.js')
 
-const HOST = process.env.HOST || '127.0.0.1'
-const PORT = process.env.PORT || 8080
-const app = express()
+const s3 = new S3Client({})
 
-app.use('/api', routes)
+exports.handler = async event => {
+  const bucket = event.Records[0].s3.bucket.name
+  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '))
+  const params = { Bucket: bucket, Key: key }
+  const { ContentType, Body } = await s3.send(new GetObjectCommand(params))
 
-app.listen(PORT, HOST, () => {
-  console.log(`Server started at http://${HOST}:${PORT}`)
-})
+  if (ContentType !== 'Buffer') {
+    return { message: 'File is not a CSV' }
+  }
+
+  const buffer = Buffer.from(Body.data)
+  const created = await csv(buffer, async line => {
+    try {
+      const json = hubspot.schema(line)
+      await hubspot.uploadContact(json)
+    } catch (e) {
+      console.log(e)
+    }
+  })
+
+  return { message: 'done', created }
+}
